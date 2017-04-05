9476400af504fd546d5e7959fbddd0cf5c87806a
Filip <koprivec.filip@gmail.com>
2017-04-05 21:37:04 +0200

    Added new tests

--- naloge/dn1/module_test.py
+++ naloge/dn1/module_test.py
@@ -6,13 +6,24 @@ from time import process_time
 import random
 
 
-def test_adding(tree: AbstractTree, n: int = 10 ** 4) -> None:
-    for j in range(n):
+def test_adding(tree: AbstractTree, n: int = 10**4, shuffle: bool = True) -> None:
+    ran = list(range(n))
+    if shuffle:
+        random.shuffle(ran)
+    for j in ran:
         tree.insert(j)
+    tree.insert(-1)
+    tree.insert(-1)
+    tree.insert(-1)
     for j in range(n):
         assert j in tree
+    assert -1 in tree
     for j in range(n):
         tree.remove(j)
+    for j in range(n):
+        assert j not in tree
+    tree.remove(-1)
+    assert -1 not in tree
 
 
 def main() -> None:
