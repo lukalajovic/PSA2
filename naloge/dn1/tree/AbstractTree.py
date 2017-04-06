a621f44e32f20514e8c4e71e26172dbed5afdeec
Filip <koprivec.filip@gmail.com>
2017-04-06 19:55:31 +0200

    Added AbstractSearchTree and corresponding TreeDict

--- naloge/dn1/tree/AbstractTree.py
+++ naloge/dn1/tree/AbstractTree.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from typing import Generic, Iterable, TypeVar, Optional
+from typing import Generic, Iterable, TypeVar, Optional, Iterator, MutableMapping
 
 T = TypeVar("T")
 
@@ -34,3 +34,33 @@ class AbstractTree(Generic[T]):
 
     def __contains__(self, item: T) -> bool:
         return self.search(item)
+
+"""
+Če implementirate metode v tem drevesu, potem lahko drevo s pomočjo TreeDict brez težav pretvorimo v slovar, 
+to pa vsekakor ni pogoj za domačo nalogo.
+"""
+class AbstractSearchTree(AbstractTree, Generic[T]):
+
+    def find(self, item: T) -> T:
+        """
+        Vrne vrednost v drevesu, ki je enaka item, če se item nahaja v drevesu, drugače pa sproži ValueError
+        :param item: Element, ki ga iščemo
+        :return: Element v drevesu
+        """
+        raise NotImplementedError("Not implemented")
+
+    def search(self, item: T) -> bool:
+        try:
+            self.find(item)
+            return True
+        except ValueError:
+            return False
+
+    def in_order_traversal(self) -> Iterator[T]:
+        raise NotImplementedError("Not implemented")
+
+    def size(self) -> int:
+        raise NotImplementedError("Not implemented")
+
+    def __len__(self) -> int:
+        return self.size()
